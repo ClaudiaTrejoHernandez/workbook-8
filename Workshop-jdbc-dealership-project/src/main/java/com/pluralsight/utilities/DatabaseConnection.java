35c549dff9eef7a565ba94555ee641cd82f0c112
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-06-11 11:10:57 -0700

    Created a class to have the Database Connection separate
    -This will keep my code clean and separated
    -it'll be easier to find and change if need be

---
+++ Workshop-jdbc-dealership-project/src/main/java/com/pluralsight/utilities/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package com.pluralsight.utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:sqlserver://skills4it.database.windows.net:1433;" +
+            "database=Courses;" +
+            "user=gtareader@skills4it;" +
+            "password=StrongPass!2025;" +
+            "encrypt=true;" +
+            "trustServerCertificate=false;" +
+            "loginTimeout=30;";
+
+    private DatabaseConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+
+}
