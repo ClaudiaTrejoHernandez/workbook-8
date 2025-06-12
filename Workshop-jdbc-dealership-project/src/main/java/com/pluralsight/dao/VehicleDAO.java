224403730457d95c6ba16801c5200a5fe5294b5c
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-06-12 11:02:15 -0700

    Update to VehicleDAO
    --logic and syntax changes to this DAO
    --creating methods required to allow a user to search the database for vehicles

--- Workshop-jdbc-dealership-project/src/main/java/com/pluralsight/dao/VehicleDAO.java
+++ Workshop-jdbc-dealership-project/src/main/java/com/pluralsight/dao/VehicleDAO.java
@@ -1,17 +1,50 @@
 package com.pluralsight.dao;
 
 import com.pluralsight.models.Vehicle;
+import com.pluralsight.utilities.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class VehicleDAO {
-    private Connection conn;
+    private final Connection conn;
 
-    public VehicleDAO(Connection conn) {
-        this.conn = conn;
+    public VehicleDAO() throws SQLException {
+        this.conn = DatabaseConnection.getConnection();
+    }
+
+    public List<Vehicle> getByPriceRange (double min, double max) throws SQLException {
+
+//Making a query in Java
+        String sql = "SELECT * FROM CT.vehicles WHERE price BETWEEN ? AND ?";
+        List<Vehicle> results = new ArrayList<>();
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setDouble(1, min);
+            stmt.setDouble(2, max);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    results.add(new Vehicle(
+                            rs.getString("VIN"),
+                            rs.getString("make"),
+                            rs.getString("model"),
+                            rs.getInt("year"),
+                            rs.getString("color"),
+                            rs.getInt("odometer"),
+                            rs.getDouble("price"),
+                            rs.getString("vehicle_Type"),
+                            rs.getBoolean("sold")
+                    ));
+                }
+
+            }
+
+        }
+        return results;
+
     }
 
     public void saveVehicle(Vehicle vehicle) throws SQLException {
@@ -25,22 +58,37 @@ public class VehicleDAO {
         }
     }
 
-    public Vehicle getVehicleByVIN(String VIN) throws SQLException {
-        String sql = "SELECT * FROM CT.Vehicles WHERE VIN = ?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, VIN);
-            try (ResultSet rs = stmt.executeQuery()) {
-
-                if (rs.next()) {
-                    return new Vehicle(
-                            rs.getString("VIN"),
-                            rs.getString("make"),
-                            rs.getString("model"),
-                            rs.getBoolean("sold"));
-                }
-                return null;
-
-            }
-        }
+    public void getByMakeModel() {
     }
+
+    public void getByYear() {
+    }
+
+    public void getByColor() {
+    }
+
+    public void getByMileage() {
+    }
+
+    public void getByVehicleType() {
+    }
+
+//    public Vehicle getVehicleByVIN(String VIN) throws SQLException {
+//        String sql = "SELECT * FROM CT.Vehicles WHERE VIN = ?";
+//        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+//            stmt.setString(1, VIN);
+//            try (ResultSet rs = stmt.executeQuery()) {
+//
+//                if (rs.next()) {
+//                    return new Vehicle(
+//                            rs.getString("VIN"),
+//                            rs.getString("make"),
+//                            rs.getString("model"),
+//                            rs.getBoolean("sold"));
+//                }
+//                return null;
+//
+//            }
+//        }
+//    }
 }
