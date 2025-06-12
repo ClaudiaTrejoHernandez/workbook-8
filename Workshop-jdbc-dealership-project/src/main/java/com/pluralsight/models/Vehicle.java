a9fb883091c236a178bf059c3a94a1ace9ea13b6
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-06-12 11:06:09 -0700

    Updates to the vehicles to reflect the database table
    -added more variables, updated the constructor, updated getters and setters, updated the toString override to reflect those changes too

--- Workshop-jdbc-dealership-project/src/main/java/com/pluralsight/models/Vehicle.java
+++ Workshop-jdbc-dealership-project/src/main/java/com/pluralsight/models/Vehicle.java
@@ -5,14 +5,24 @@ public class Vehicle {
     private String vin;
     private String make;
     private String model;
+    private int year;
+    private String color;
+    private int odometer;
+    private double price;
+    private String vehicleType;
 
-    private boolean isSold = false;
+    private boolean sold = false;
 
-    public Vehicle(String vin, String make, String model, boolean isSold) {
+    public Vehicle(String vin, String make, String model, int year, String color, int odometer, double price, String vehicleType, boolean isSold) {
         this.vin = vin;
         this.make = make;
         this.model = model;
-        this.isSold = isSold;
+        this.year = year;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+        this.vehicleType = vehicleType;
+        this.sold = isSold;
     }
 
     public String getVin() {
@@ -39,22 +49,63 @@ public class Vehicle {
         this.model = model;
     }
 
-    public void setSold(boolean sold) {
-        isSold = sold;
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
     }
 
     public boolean isSold() {
-        return isSold;
+        return sold;
+    }
+
+    public void setSold(boolean sold) {
+        this.sold = sold;
     }
 
     public String getStatus() {
-        return isSold ? "Sold" : "Available";
+        return sold ? "Sold" : "Available";
     }
 
+    @Override
     public String toString() {
-        return String.format("Status: %s | VIN: %d | Make: %s | Model: %s | ",
-                getStatus(), vin, make, model);    }
-
-
+        return String.format(
+                "Status: %-9s | VIN: %-10s | Make: %-10s | Model: %-10s | Year: %-4d | Color: %-10s | Odometer: %,8d mi | Price: $%,10.2f | Type: %-10s",
+                getStatus(), vin, make, model, year, color, odometer, price, vehicleType);
+    }
 
 }
